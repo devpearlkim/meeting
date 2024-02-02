2d3d363f36cff2cce283776bfd5e4e41630d5ad9
jay <valenqbd02@gmail.com>
2024-02-03 01:10:26 +0900

    chore: 포트번호 변경

--- src/routes.tsx
+++ src/routes.tsx
@@ -6,8 +6,8 @@ import NewUsers from './pages/Users'
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/signup" element={<NewUsers />} />
-      <Route path="/login" element={<Login />} />
+      <Route path="http://localhost:5173/signup" element={<NewUsers />} />
+      <Route path="http://localhost:5173/login" element={<Login />} />
       <Route element={<AppLayout />}>
         {/* Default route when the path is "/". */}
       </Route>
