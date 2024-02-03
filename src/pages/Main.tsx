495a2820dfb6339b06b17485237f7db77677e2d0
jay <valenqbd02@gmail.com>
2024-02-03 15:25:07 +0900

    chore: 메인페이지 추가(엔진엑스 테스트용)

---
+++ src/pages/Main.tsx
@@ -0,0 +1,5 @@
+const Main = () => {
+  return <div>안녕하세요 메인페이지입니다</div>
+}
+
+export default Main
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,10 +2,12 @@ import { Route, Routes } from 'react-router-dom'
 import AppLayout from './ui/AppLayout'
 import Login from './pages/Login'
 import NewUsers from './pages/Users'
+import Main from './pages/Main'
 
 export const AppRoutes = () => {
   return (
     <Routes>
+      <Route path="/" element={<Main />} />
       <Route path="/signup" element={<NewUsers />} />
       <Route path="/login" element={<Login />} />
       <Route element={<AppLayout />}>
