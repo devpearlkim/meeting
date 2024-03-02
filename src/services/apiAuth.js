595a76abe2db0ec44552125ab482104130d3dd0a
jay <valenqbd02@gmail.com>
2024-03-02 14:41:39 +0900

    chore: backend에서 token줄때 Barear뒤에 공백 추가로 인한 테스트

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -82,9 +82,7 @@ export async function login({ email, password }) {
     })
     const token = response.headers.get('Authorization')
 
-    console.log(token.startsWith('Bearer '))
-    if (token && token.startsWith('Bearer ')) {
-      console.log('if문')
+    if (token) {
       sessionStorage.setItem('token', token.replace('Bearer ', ''))
     }
 
