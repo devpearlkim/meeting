fa4162c65cfc9223e9b25dd32892cfb651112b46
jay <valenqbd02@gmail.com>
2024-03-02 19:32:45 +0900

    chore: 프로필에서 userInfo가져오기

--- src/features/myPage/Profile.tsx
+++ src/features/myPage/Profile.tsx
@@ -1,6 +1,6 @@
 const Profile = () => {
   const userInfo = sessionStorage.getItem('userInfo')
-  console.log(userInfo)
+  console.log(JSON.stringify(userInfo))
 
   return <div className="bg-pink">프로필이미지랑자기소개</div>
 }
