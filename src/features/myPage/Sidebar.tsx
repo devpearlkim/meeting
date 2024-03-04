ae0c696d39e2dcf49ac6a2a55dc78ed2240564c7
jay <valenqbd02@gmail.com>
2024-03-04 21:41:34 +0900

    chore: 마이페이지 탭으로 userId넘기기

--- src/features/myPage/Sidebar.tsx
+++ src/features/myPage/Sidebar.tsx
@@ -1,13 +1,29 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const Sidebar = ({ path, userId }) => {
   const listStyle = 'p-3 text-slate-700 font-semibold hover:text-blue-300'
-
-  const sidebarItems = [
-    { path: `/profile/${userId}`, text: '모임정보' },
-    { path: '/applies', text: '신청내역' },
-    { path: '/bookmark', text: '북마크' },
-  ]
+  const [isMyProfile, setIsMyProfile] = useState(
+    JSON.parse(sessionStorage.getItem('userInfo')).userId === userId
+      ? true
+      : false,
+  )
+  let sidebarItems = isMyProfile
+    ? [
+        {
+          path: `/profile/${JSON.parse(sessionStorage.getItem('userInfo')).userId}`,
+          text: '모임정보',
+        },
+        {
+          path: `/applies/${JSON.parse(sessionStorage.getItem('userInfo')).userId}`,
+          text: '신청내역',
+        },
+        {
+          path: `/bookmark/${JSON.parse(sessionStorage.getItem('userInfo')).userId}`,
+          text: '북마크',
+        },
+      ]
+    : [{ path: `/profile/${userId}`, text: '모임정보' }]
 
   return (
     <div className="">
