1608392c805d13027d43a076094daf41b4cba4af
jay <valenqbd02@gmail.com>
2024-03-04 12:58:39 +0900

    chore: 콘솔에서 searchBarInput확인

--- src/features/meetings/SearchBarInput.tsx
+++ src/features/meetings/SearchBarInput.tsx
@@ -2,17 +2,19 @@ import { useFormContext } from 'react-hook-form'
 import { useLocation } from 'react-router-dom'
 
 const SearchBarInput = () => {
-  const { register } = useFormContext()
+  const { register, setValue } = useFormContext()
   const searchParams = new URLSearchParams(useLocation().search)
   const initialSearch = searchParams.get('search') ?? ''
   console.log(initialSearch)
+  setValue('search', initialSearch)
+
   return (
     <div className="">
       <input
         type="text"
         {...register('search')}
         placeholder="키워드입력"
-        defaultValue={initialSearch}
+        // defaultValue={initialSearch}
         className="w-48 rounded-l-3xl border-r-2 p-4 font-semibold outline-none hover:rounded-3xl hover:bg-slate-100"
       />
     </div>
