6b14b566520b273ade2418879571e745937d2cae
jay <valenqbd02@gmail.com>
2024-03-04 21:15:31 +0900

    chore: 신고모달 분리 id넘기기 확인

--- src/features/myPage/Tab.tsx
+++ src/features/myPage/Tab.tsx
@@ -1,13 +1,14 @@
 import { Link } from 'react-router-dom'
 
-const sidebarItems = [
-  { path: '/profile', text: '모임정보' },
-  { path: '/applies', text: '신청내역' },
-  { path: '/bookmark', text: '북마크' },
-]
-
 const Sidebar = ({ path }: { path: string }) => {
   const listStyle = 'p-3 text-slate-700 font-semibold hover:text-blue-300'
+
+  console.log('사이드바에서의 path', path)
+  const sidebarItems = [
+    { path: '/profile', text: '모임정보' },
+    { path: '/applies', text: '신청내역' },
+    { path: '/bookmark', text: '북마크' },
+  ]
 
   return (
     <div className="">
