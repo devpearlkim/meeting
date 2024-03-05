fc8516134179ad0cb05988dddaa71b61a673f56f
jay <valenqbd02@gmail.com>
2024-03-05 20:59:54 +0900

    chore: 신고, 미팅참가 api연결

--- src/pages/PostDetail.tsx
+++ src/pages/PostDetail.tsx
@@ -74,7 +74,7 @@ const postDetail = () => {
     const meetingParticipants = await getMeetingParicipants(data.data.meetingId)
     console.log('신청자 목록')
     console.log(meetingParticipants)
-    getMeetingParicipants?.data.some((participant) =>
+    getMeetingParicipants?.data?.some((participant) =>
       console.log(participant.userid),
     )
     console.log('로그인id', loggedInUserId)
