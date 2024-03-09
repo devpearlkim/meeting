0bb87405cd894cdb196482aaba0778b5f4519995
jay <valenqbd02@gmail.com>
2024-03-10 00:39:16 +0900

    feat: nav바 만들기

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -1,5 +1,32 @@
+import { Link } from 'react-router-dom'
+
 const Header = () => {
-  return <div>헤더입니다.</div>
+  const userInfo = JSON.parse(sessionStorage.getItem('userInfo'))
+  const userId = userInfo.userId
+
+  const handleLogout = () => {
+    console.log('로그아웃 버튼 클릭')
+  }
+
+  return (
+    <div className="flex justify-between">
+      <Link to={'/'}>
+        <div>로고</div>
+      </Link>
+      <div className="flex justify-end">
+        {userId ? (
+          <Link to={`/profile/${userId}`}>
+            <span>마이페이지아이콘</span>
+          </Link>
+        ) : (
+          <Link to={'/login'}>
+            <button>로그인버튼</button>
+          </Link>
+        )}
+        {userId && <button onClick={handleLogout}>로그인버튼</button>}
+      </div>
+    </div>
+  )
 }
 
 export default Header
