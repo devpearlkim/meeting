a5f91c7e43127898f86fd76b5777eaf9ae6df22a
jay <valenqbd02@gmail.com>
2024-03-09 23:41:08 +0900

    chore: 좋아요 반영 확인

--- src/features/meetings/Post.tsx
+++ src/features/meetings/Post.tsx
@@ -2,7 +2,7 @@ import { Link, useNavigate } from 'react-router-dom'
 import { PiSirenLight } from 'react-icons/pi'
 import { useEffect, useState } from 'react'
 import { addLike, deleteLike } from '../../services/apiPost'
-import { QueryCache, useMutation } from '@tanstack/react-query'
+import { queryCache } from '@tanstack/react-query'
 
 const Post = ({ post, setShowModal, reportedPostId, setReportedPostId }) => {
   const navigate = useNavigate()
@@ -16,31 +16,18 @@ const Post = ({ post, setShowModal, reportedPostId, setReportedPostId }) => {
     setShowModal(true)
   }
 
-  const [addLikeMutation] = useMutation(addLike, {
-    onSuccess: () => {
-      QueryCache.invalidateQueries(['posts'])
-    },
-  })
+  const deleteLikeAPI = () => {
+    !isLogin && navigate('/login')
+    deleteLike(post.meetingId)
+    setIsLiked(false)
+    queryCache.invalidateQueries((key) => key.startsWith('posts'))
+  }
 
-  const [deleteLikeMutation] = useMutation(deleteLike, {
-    onSuccess: () => {
-      queryCache.invalidateQueries(['posts'])
-    },
-  })
-
-  const handleLikeClick = () => {
-    if (!isLogin) {
-      navigate('/login')
-      return
-    }
-
-    if (isLiked) {
-      deleteLikeMutation(post.meetingId)
-    } else {
-      addLikeMutation(post.meetingId)
-    }
-
-    setIsLiked(!isLiked)
+  const addLikeAPI = () => {
+    !isLogin && navigate('/login')
+    addLike(post.meetingId)
+    setIsLiked(true)
+    queryCache.invalidateQueries((key) => key.startsWith('posts'))
   }
 
   return (
@@ -88,7 +75,7 @@ const Post = ({ post, setShowModal, reportedPostId, setReportedPostId }) => {
           </div>
         </Link>
         <div className="mt-auto flex items-center justify-between gap-4 border-t border-slate-300 px-2 pb-2 pt-4">
-          <button onClick={handleLikeClick}>
+          <button onClick={isLiked ? deleteLikeAPI : addLikeAPI}>
             {isLiked ? '꽉찬하트' : '빈하트'}
           </button>
           <button onClick={handleReportClick}>
