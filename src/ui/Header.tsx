97531d1ba8b1f66196b435eafd6ed48017ed9b25
jay <valenqbd02@gmail.com>
2024-03-10 00:43:58 +0900

    feat: nav바 만들기

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -14,15 +14,17 @@ const Header = () => {
         <div>로고</div>
       </Link>
       <div className="flex justify-end gap-10">
-        {userId ? (
-          <Link to={`/profile/${userId}`}>
-            <span>마이페이지아이콘</span>
-          </Link>
-        ) : (
-          <Link to={'/login'}>
-            <button>로그인버튼</button>
-          </Link>
-        )}
+        <div>
+          {userId ? (
+            <Link to={`/profile/${userId}`}>
+              <span>마이페이지아이콘</span>
+            </Link>
+          ) : (
+            <Link to={'/login'}>
+              <button>로그인버튼</button>
+            </Link>
+          )}
+        </div>
         {userId && <button onClick={handleLogout}>로그아웃버튼</button>}
       </div>
     </div>
