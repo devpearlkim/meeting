c1a4f322ce5155d91a27f75616812eaab128e712
jay <valenqbd02@gmail.com>
2024-03-09 19:24:14 +0900

    chore: 프로필정보조회

--- src/features/myPage/Profile.tsx
+++ src/features/myPage/Profile.tsx
@@ -5,17 +5,35 @@ import { getUserProfile } from '../../services/apiUser'
 const Profile = () => {
   const { userId } = useParams()
 
-  const { data, isFetching } = useQuery({
+  const { data } = useQuery({
     queryKey: ['profile', userId],
     queryFn: getUserProfile,
   })
 
   console.log('프로필정보', data)
 
+  const editProfile = () => {
+    console.log('프로필 수정 버튼 클릭')
+  }
+
   return (
-    <div className="bg-pink">
-      프로필이미지, 닉네임, 프로필 수정버튼(회원가입폼으로 이동후 patch요청
-      일반회원시만 비번변경)
+    <div className="flex flex-col">
+      <div className="flex gap-2">
+        <img
+          className="h-40 w-40 rounded-full"
+          src={data?.profileImage}
+          alt="프로필이미지"
+        />
+        <span className="block text-lg font-semibold">{data?.nickname}</span>
+      </div>
+      <div className="flex gap-1">
+        {data?.categories.map((category) => (
+          <button className="inline-block rounded-full bg-blue-500 px-3 py-1 text-xs text-white">
+            {category.name}
+          </button>
+        ))}
+      </div>
+      <button onClick={editProfile}>프로필 수정</button>
     </div>
   )
 }
