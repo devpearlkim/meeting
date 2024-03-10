c6f9acd99f70c17ae4dda94b0ac1e9e4a5d5eaa7
jay <valenqbd02@gmail.com>
2024-03-10 15:30:18 +0900

    feat: List gap조절

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -87,7 +87,7 @@ const List = () => {
             </div>
 
             {data?.pages.map((page, i) => (
-              <div key={i} className="flex flex-wrap gap-2">
+              <div key={i} className="flex flex-wrap gap-8">
                 {page.data?.map((post) => (
                   <Post
                     post={post}
