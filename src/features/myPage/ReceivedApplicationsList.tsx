6d61da26e3f8cf769500ffb471e246c2096ad552
jay <valenqbd02@gmail.com>
2024-03-10 17:51:55 +0900

    feat: 신청받은 내역 조회

--- src/features/myPage/ReceivedApplicationsList.tsx
+++ src/features/myPage/ReceivedApplicationsList.tsx
@@ -1,5 +1,28 @@
+import { useQuery, useQueryClient } from '@tanstack/react-query'
+import { getCreatedMeetingIds } from '../../services/apiPost'
+import { getMeetingParicipants } from '../../services/apiParticipant'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+
 const ReceivedApplicationsList = () => {
-  return <>신청받은목록</>
+  const [page, setPage] = useState(1)
+
+  const { data: meetingIds } = useQuery({
+    queryKey: ['createdMeetingIds'],
+    queryFn: getCreatedMeetingIds,
+  })
+
+  const participantQueries = meetingIds?.map((meetingId) => {
+    return useQuery({
+      queryKey: ['meetingParticipants', meetingId],
+      queryFn: () => getMeetingParicipants(meetingId),
+    })
+  })
+
+  console.log('신청받은내역', participantQueries)
+  // console.log()
+
+  return <div>신청받은내역</div>
 }
 
 export default ReceivedApplicationsList
