f611284975686e3d613453ffdb3bdc55a84a15d8
jay <valenqbd02@gmail.com>
2024-03-15 10:37:57 +0900

    chore: nav css조정

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -10,24 +10,29 @@ const Header = () => {
   }
 
   return (
-    <div className="mx-44 flex justify-between px-20">
+    <div className="mx-44 flex justify-between px-20 py-4">
       <Link to={'/'}>
-        <h1 className="text-2xl font-bold text-cyan-400">JW</h1>
+        <h1 className="text-2xl font-bold">JW</h1>
       </Link>
       <div className="flex justify-end gap-10">
         <div>
           {userId ? (
             <Link to={`/profile/${userId}`}>
-              <button className="font-semibold">마이페이지</button>
+              <button className="mt-2 font-semibold text-neutral-800">
+                마이페이지
+              </button>
             </Link>
           ) : (
             <Link to={'/login'}>
-              <button className="font-semibold">로그인</button>
+              <button className="font-semibold text-neutral-800">로그인</button>
             </Link>
           )}
         </div>
         {userId && (
-          <button className="font-semibold" onClick={handleLogout}>
+          <button
+            className="font-semibold text-neutral-800"
+            onClick={handleLogout}
+          >
             로그아웃
           </button>
         )}
